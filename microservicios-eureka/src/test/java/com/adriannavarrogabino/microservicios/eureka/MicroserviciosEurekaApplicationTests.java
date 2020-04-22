798358ad1396e2c07fabd77c29424487f62d5614
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-04-22 16:41:25 +0200

    Creando servidor de registro Eureka

---
+++ microservicios-eureka/src/test/java/com/adriannavarrogabino/microservicios/eureka/MicroserviciosEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adriannavarrogabino.microservicios.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
