28858b7c9c4d02b3718e57e7fe1cb629eecfa713
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-04-22 17:13:57 +0200

    Creando y configurando servidor Zuul Gateway

---
+++ microservicios-zuul/src/test/java/com/adriannavarrogabino/microservicios/zuul/MicroserviciosZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adriannavarrogabino.microservicios.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
