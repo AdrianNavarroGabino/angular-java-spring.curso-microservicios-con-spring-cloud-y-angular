47cceb0828252279152a39909d67b0faff3b758f
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-04-22 16:53:22 +0200

    Conectando servicio usuarios como cliente de Eureka

--- microservicios-usuarios/src/main/java/com/adriannavarrogabino/microservicios/usuarios/MicroserviciosUsuariosApplication.java
+++ microservicios-usuarios/src/main/java/com/adriannavarrogabino/microservicios/usuarios/MicroserviciosUsuariosApplication.java
@@ -2,7 +2,10 @@ package com.adriannavarrogabino.microservicios.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+// No es necesaria esta anotación porque ya está en el pom, pero siempre es mejor hacerlo explícito
+@EnableEurekaClient
 @SpringBootApplication
 public class MicroserviciosUsuariosApplication {
 
