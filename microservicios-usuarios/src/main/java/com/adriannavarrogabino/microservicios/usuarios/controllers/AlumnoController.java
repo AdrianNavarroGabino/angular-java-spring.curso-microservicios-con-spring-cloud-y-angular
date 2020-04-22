61faa59c1422691d584cd150700ecc5caa4535ad
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-04-22 15:55:37 +0200

    Implementando CRUD en el controlador métodos para post, put y delete

--- microservicios-usuarios/src/main/java/com/adriannavarrogabino/microservicios/usuarios/controllers/AlumnoController.java
+++ microservicios-usuarios/src/main/java/com/adriannavarrogabino/microservicios/usuarios/controllers/AlumnoController.java
@@ -1,9 +1,14 @@
 package com.adriannavarrogabino.microservicios.usuarios.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.adriannavarrogabino.microservicios.usuarios.models.entity.Alumno;
@@ -32,5 +37,45 @@ public class AlumnoController {
 		}
 		
 		return ResponseEntity.ok().body(alumno);
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> crear(@RequestBody Alumno alumno)
+	{
+		Alumno alumnoDb = alumnoService.save(alumno);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(alumnoDb);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> editar(@RequestBody Alumno alumno, @PathVariable Long id)
+	{
+		Alumno alumnoDb = alumnoService.findById(id);
+		
+		if(alumnoDb == null)
+		{
+			return ResponseEntity.notFound().build();
+		}
+		
+		alumnoDb.setNombre(alumno.getNombre());
+		alumnoDb.setApellido(alumno.getApellido());
+		alumnoDb.setEmail(alumno.getEmail());
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(alumnoService.save(alumnoDb));
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> eliminar(@PathVariable Long id)
+	{
+		Alumno alumnoDb = alumnoService.findById(id);
+		
+		if(alumnoDb == null)
+		{
+			return ResponseEntity.notFound().build();
+		}
+		
+		alumnoService.deleteById(id);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
