cfe0e9eddf17b957ea632511b708e253de459970
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-04-22 12:47:00 +0200

    Creando nuestro microservicio usuarios

---
+++ microservicios-usuarios/src/main/java/com/adriannavarrogabino/microservicios/usuarios/MicroserviciosUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.adriannavarrogabino.microservicios.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosUsuariosApplication.class, args);
+	}
+
+}
---
+++ microservicios-usuarios/src/test/java/com/adriannavarrogabino/microservicios/usuarios/MicroserviciosUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adriannavarrogabino.microservicios.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
