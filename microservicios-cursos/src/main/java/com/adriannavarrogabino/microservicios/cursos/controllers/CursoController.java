0aa7a80fad539af66e45ef701cf7ae17ce1307ac
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-04-23 10:19:58 +0200

    Asignar y eliminar alumnos al curso

--- microservicios-cursos/src/main/java/com/adriannavarrogabino/microservicios/cursos/controllers/CursoController.java
+++ microservicios-cursos/src/main/java/com/adriannavarrogabino/microservicios/cursos/controllers/CursoController.java
@@ -1,5 +1,7 @@
 package com.adriannavarrogabino.microservicios.cursos.controllers;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.adriannavarrogabino.microservicios.commons.alumnos.models.entity.Alumno;
 import com.adriannavarrogabino.microservicios.commons.controllers.CommonController;
 import com.adriannavarrogabino.microservicios.cursos.models.entity.Curso;
 import com.adriannavarrogabino.microservicios.cursos.models.services.ICursoService;
@@ -24,6 +27,36 @@ public class CursoController extends CommonController<Curso, ICursoService> {
 		}
 		
 		cursoDb.setNombre(curso.getNombre());
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(cursoDb));
+	}
+	
+	@PutMapping("/{id}/asignar-alumnos")
+	public ResponseEntity<?> asignarAlumnos(@RequestBody List<Alumno> alumnos, @PathVariable Long id) {
+		
+		Curso cursoDb = this.service.findById(id);
+		
+		if(cursoDb == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		alumnos.forEach(a -> {
+			cursoDb.addAlumno(a);
+		});
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(cursoDb));
+	}
+	
+	@PutMapping("/{id}/eliminar-alumno")
+	public ResponseEntity<?> eliminarAlumno(@RequestBody Alumno alumno, @PathVariable Long id) {
+		
+		Curso cursoDb = this.service.findById(id);
+		
+		if(cursoDb == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		cursoDb.removeAlumno(alumno);
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(cursoDb));
 	}
