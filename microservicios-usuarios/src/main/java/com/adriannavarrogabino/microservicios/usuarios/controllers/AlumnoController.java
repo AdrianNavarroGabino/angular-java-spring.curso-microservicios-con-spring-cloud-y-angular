771bda57d378be01a2fdf4c9146934c77406b4dc
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-04-24 08:59:07 +0200

    Añadeindo métodos handlers con soporte multipartfile para post y put

--- microservicios-usuarios/src/main/java/com/adriannavarrogabino/microservicios/usuarios/controllers/AlumnoController.java
+++ microservicios-usuarios/src/main/java/com/adriannavarrogabino/microservicios/usuarios/controllers/AlumnoController.java
@@ -1,5 +1,7 @@
 package com.adriannavarrogabino.microservicios.usuarios.controllers;
 
+import java.io.IOException;
+
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
@@ -7,9 +9,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.adriannavarrogabino.microservicios.commons.alumnos.models.entity.Alumno;
 import com.adriannavarrogabino.microservicios.commons.controllers.CommonController;
@@ -43,5 +48,45 @@ public class AlumnoController extends CommonController<Alumno, IAlumnoService> {
 	public ResponseEntity<?> filtrar(@PathVariable String term) {
 		
 		return ResponseEntity.ok().body(service.findByNombreOApellido(term));
+	}
+
+	@PostMapping("/crear-con-foto")
+	public ResponseEntity<?> crearConFoto(
+			@Valid Alumno alumno, BindingResult result,
+			@RequestParam MultipartFile archivo) throws IOException {
+		
+		if(!archivo.isEmpty())
+		{
+			alumno.setFoto(archivo.getBytes());
+		}
+		return super.crear(alumno, result);
+	}
+	
+	@PutMapping("/editar-con-foto/{id}")
+	public ResponseEntity<?> editarConFoto(
+			@Valid Alumno alumno, BindingResult result, @PathVariable Long id,
+			@RequestParam MultipartFile archivo) throws IOException
+	{
+		if(result.hasErrors()) {
+			return validar(result);
+		}
+		
+		Alumno alumnoDb = service.findById(id);
+		
+		if(alumnoDb == null)
+		{
+			return ResponseEntity.notFound().build();
+		}
+		
+		alumnoDb.setNombre(alumno.getNombre());
+		alumnoDb.setApellido(alumno.getApellido());
+		alumnoDb.setEmail(alumno.getEmail());
+		
+		if(!archivo.isEmpty())
+		{
+			alumnoDb.setFoto(archivo.getBytes());
+		}
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(alumnoDb));
 	}
 }
