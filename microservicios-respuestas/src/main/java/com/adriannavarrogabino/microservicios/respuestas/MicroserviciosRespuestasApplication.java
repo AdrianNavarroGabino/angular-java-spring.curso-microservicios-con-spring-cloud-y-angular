3b217d84be1125df00c1b584b62ef8b2a6474ab6
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-04-24 09:15:26 +0200

    Configurando entity scan

--- microservicios-respuestas/src/main/java/com/adriannavarrogabino/microservicios/respuestas/MicroserviciosRespuestasApplication.java
+++ microservicios-respuestas/src/main/java/com/adriannavarrogabino/microservicios/respuestas/MicroserviciosRespuestasApplication.java
@@ -2,10 +2,14 @@ package com.adriannavarrogabino.microservicios.respuestas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EntityScan({"com.adriannavarrogabino.microservicios.respuestas.models.entity",
+	"com.adriannavarrogabino.microservicios.commons.alumnos.models.entity",
+	"com.adriannavarrogabino.microservicios.commons.examenes.models.entity"})
 public class MicroserviciosRespuestasApplication {
 
 	public static void main(String[] args) {
