3d373f409a56807f3df98a31493e16b576ffa1a6
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-04-24 09:22:23 +0200

    Añadiendo entity Respuesta y sus relaciones con el alumno y la pregunta

--- microservicios-respuestas/src/main/java/com/adriannavarrogabino/microservicios/respuestas/models/entity/Respuesta.java
+++ microservicios-respuestas/src/main/java/com/adriannavarrogabino/microservicios/respuestas/models/entity/Respuesta.java
@@ -1,21 +1,33 @@
 package com.adriannavarrogabino.microservicios.respuestas.models.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import org.springframework.data.annotation.Id;
 
+import com.adriannavarrogabino.microservicios.commons.alumnos.models.entity.Alumno;
+import com.adriannavarrogabino.microservicios.commons.examenes.models.entity.Pregunta;
+
 @Entity
 @Table(name = "respuestas")
 public class Respuesta {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String texto;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Alumno alumno;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	private Pregunta pregunta;
 
 	public Long getId() {
 		return id;
@@ -32,6 +44,21 @@ public class Respuesta {
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
-	
-	
+
+	public Alumno getAlumno() {
+		return alumno;
+	}
+
+	public void setAlumno(Alumno alumno) {
+		this.alumno = alumno;
+	}
+
+	public Pregunta getPregunta() {
+		return pregunta;
+	}
+
+	public void setPregunta(Pregunta pregunta) {
+		this.pregunta = pregunta;
+	}
+
 }
