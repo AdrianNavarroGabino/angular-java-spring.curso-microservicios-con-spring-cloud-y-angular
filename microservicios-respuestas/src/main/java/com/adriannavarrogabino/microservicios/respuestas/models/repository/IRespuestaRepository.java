aa5705bec48afa42934dc742f74d0f2222251cbb
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-04-24 09:33:06 +0200

    Escribiendo consulta con join para las respuestas del alumno por examen

--- microservicios-respuestas/src/main/java/com/adriannavarrogabino/microservicios/respuestas/models/repository/IRespuestaRepository.java
+++ microservicios-respuestas/src/main/java/com/adriannavarrogabino/microservicios/respuestas/models/repository/IRespuestaRepository.java
@@ -1,9 +1,13 @@
 package com.adriannavarrogabino.microservicios.respuestas.models.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.adriannavarrogabino.microservicios.respuestas.models.entity.Respuesta;
 
 public interface IRespuestaRepository extends CrudRepository<Respuesta, Long> {
+	
+	@Query("select r from Respuesta r join fetch r.alumno a join fetch r.pregunta p join fetch p.examen e where a.id = ?1 and e.id = ?2")
+	public Iterable<Respuesta> findRespuestaByAlumnoByExamen(Long alumnoId, Long examenID);
 
 }
